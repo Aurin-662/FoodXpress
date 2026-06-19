ef94f6872e814078314cbe045e5c1e6607a40d51
Aurin-662 <aurin662@gmail.com>
2026-06-19 16:28:13 +0600

    made user and admin login system

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/home', [HomeController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
